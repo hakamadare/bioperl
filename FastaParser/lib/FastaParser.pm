37157a65adf98529995458361bf2d0582c803569
Steve Huff <shuff@vecna.org>
2011-06-15 19:13:26 -0400

    basic fasta parsing is working
    `curl --upload-file 23-nucleotide.txt http://localhost:3000/fasta`
    results in a bunch of fastas

--- FastaParser/lib/FastaParser.pm
+++ FastaParser/lib/FastaParser.pm
@@ -1,10 +1,34 @@
 package FastaParser;
+use IO::Scalar;
+use PerlIO::eol;
+use Tie::File::AnyData::Bio::Fasta;
 use Dancer ':syntax';
+
+set serializer => 'JSON';
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+put '/fasta' => sub {
+    my $result;
+
+    my $body = request->body;
+    defined( $body ) or return;
+
+    my $FASTAS = new IO::Scalar \$body;
+    tie my @fastas, 'Tie::File::AnyData::Bio::Fasta', $FASTAS;
+
+    foreach my $fasta ( @fastas ) {
+        my( $header, $payload ) = split( "\n", $fasta, 2 );
+        $payload =~ s/\n//g;
+
+        $result->{$header} = $payload;
+    }
+
+    return( $result );
 };
 
 true;
