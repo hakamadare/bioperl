11a131cfd2446afb28105184e34ebf7cdebf7169
Steve Huff <shuff@vecna.org>
2011-06-15 20:10:02 -0400

    working on stats analysis

--- FastaParser/lib/FastaParser.pm
+++ FastaParser/lib/FastaParser.pm
@@ -18,14 +18,45 @@ put '/fasta' => sub {
     my $body = request->body;
     defined( $body ) or return;
 
+    # extract the fastas
+
     my $FASTAS = new IO::Scalar \$body;
     tie my @fastas, 'Tie::File::AnyData::Bio::Fasta', $FASTAS;
+
+    my $extracted;
 
     foreach my $fasta ( @fastas ) {
         my( $header, $payload ) = split( "\n", $fasta, 2 );
         $payload =~ s/\n//g;
 
-        $result->{$header} = $payload;
+        $extracted->{$header} = $payload;
+    }
+
+    close( $FASTAS );
+
+    # now analyze them
+    my $stats;
+    foreach my $fasta ( keys( %{$extracted} ) ) {
+        my $sequence = $extracted->{$fasta};
+        if ( defined( $sequence ) ) {
+            $stats->{total}++;
+        }
+        else {
+            next;
+        }
+
+        my( @positions ) = split( //, $sequence );
+        my $cursor = 0;
+        while ( @positions ) {
+            my $position = shift( @positions );
+            push( @{$stats->{positions}->{$cursor}->{$position}}, $fasta );
+            $cursor++;
+        }
+    }
+
+    # now summarize them
+    foreach my $position ( sort( @{$stats->{positions}} ) ) {
+        my( @line ) = ( $position );
     }
 
     return( $result );
