5d27e3e17d9349979114af00d37f7ca106c431f8
Steve Huff <shuff@vecna.org>
2011-06-15 20:45:22 -0400

    first working version

--- FastaParser/lib/FastaParser.pm
+++ FastaParser/lib/FastaParser.pm
@@ -55,8 +55,24 @@ put '/fasta' => sub {
     }
 
     # now summarize them
-    foreach my $position ( sort( @{$stats->{positions}} ) ) {
-        my( @line ) = ( $position );
+    foreach my $position ( sort {$a <=> $b } keys ( %{$stats->{positions}} ) ) {
+        my( @line ) = $position;
+        
+        my( $individual ) = ( $stats->{positions}->{$position} );
+        my( $a ) = defined( $individual->{a} ) ? scalar( @{$individual->{a}} ) : 0;
+        my( $c ) = defined( $individual->{c} ) ? scalar( @{$individual->{c}} ) : 0;
+        my( $t ) = defined( $individual->{t} ) ? scalar( @{$individual->{t}} ) : 0;
+        my( $g ) = defined( $individual->{g} ) ? scalar( @{$individual->{g}} ) : 0;
+        my( $n ) = defined( $individual->{n} ) ? scalar( @{$individual->{n}} ) : 0;
+
+        push( @line, sprintf( '%.1f', 100 * ( $a / $stats->{total} ) ) );
+        push( @line, sprintf( '%.1f', 100 * ( $c / $stats->{total} ) ) );
+        push( @line, sprintf( '%.1f', 100 * ( $t / $stats->{total} ) ) );
+        push( @line, sprintf( '%.1f', 100 * ( $g / $stats->{total} ) ) );
+        push( @line, sprintf( '%.1f', 100 * ( $n / $stats->{total} ) ) );
+        push( @line, sprintf( '%.1f', 100 * ( ( $a + $c + $t + $g + $n ) / $stats->{total} ) ) );
+
+        $result .= join( ',', @line ) . "\n";
     }
 
     return( $result );
